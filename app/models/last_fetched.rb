067d5aab0804ffba7a649de02d4cc45cc1b1a977
rlpacht <rl.pacht@gmail.com>
2015-12-15 13:15:33 -0800

    heroku fix

--- app/models/last_fetched.rb
+++ app/models/last_fetched.rb
@@ -1,4 +1,11 @@
 class LastFetched < ActiveRecord::Base
+  def self.format_time
+    if LastFetched.last.nil?
+      return 0
+    end
+    LastFetched.last.last_fetched
+    "#{t.month}/#{t.day}/#{t.year} at #{t.hour}:#{LastFetched.padded_minutes(t.min)}"
+  end
   def self.padded_minutes(minutes)
     if minutes < 10
       return "0#{minutes}"
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -21,11 +21,7 @@ class Tweet < ActiveRecord::Base
   end
 
   def self.tweets_last_fetched
-    t = LastFetched.last.last_fetched
-    if t = nil
-      return 0
-    end
-    "#{t.month}/#{t.day}/#{t.year} at #{t.hour}:#{LastFetched.padded_minutes(t.min)}"
+    LastFetched.format_time
   end
 
   def formatted_age
