ee2a6e1088985f3199e87eb87e9942cb29f9bcc8
rlpacht <rl.pacht@gmail.com>
2015-12-15 20:12:47 -0800

    fixed timezone issue

--- app/models/last_fetched.rb
+++ app/models/last_fetched.rb
@@ -1,11 +1,13 @@
+require 'date'
+require 'time'
 class LastFetched < ActiveRecord::Base
 
   def self.format_time
     if LastFetched.last.nil?
       return 0
     end
-    t = LastFetched.last.last_fetched.localtime
-    "#{t.month}/#{t.day}/#{t.year} at #{t.hour}:#{LastFetched.padded_minutes(t.min)}"
+    t = LastFetched.last.last_fetched.in_time_zone('EST')
+    "#{t.month}/#{t.day}/#{t.year} at #{t.hour - 3}:#{LastFetched.padded_minutes(t.min)}"
   end
 
   def self.padded_minutes(minutes)
